f0d0b09a27eee7dde1b6a896412602986fa9082f
Bojan Dimovski <hello@bojandimovski.com>
2024-11-06 16:59:57 +0100

    Release 1.0.0-beta.3

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DevRevSDK",
-            url: "https://github.com/devrev/devrev-sdk-ios/releases/download/v1.0.0-beta.2/DevRevSDK.xcframework.zip",
-            checksum: "dcd7b2bfc534da8cf003b080cf32c29e72f873a8212869c3074912e323f9a6a6"
+            url: "https://github.com/devrev/devrev-sdk-ios/releases/download/v1.0.0-beta.3/DevRevSDK.xcframework.zip",
+            checksum: "7443415f681b7f0c7277613a684d1148ccfec4bb786b1f2a5a1b27e9878bd0e4"
         )
     ]
 )

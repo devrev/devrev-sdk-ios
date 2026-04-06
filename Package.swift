d881c11a20e209fdc6079fb86567b7cf71aad4c1
svc-devrev <cloudops@devrev.ai>
2026-04-06 05:51:24 +0000

    Release 2.3.2

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DevRevSDK",
-            url: "https://github.com/devrev/devrev-sdk-ios/releases/download/v2.3.1/DevRevSDK.xcframework.zip",
-            checksum: "6ab85418350c58dda3e23a38583ee4a317a30712769eea09921852ccdb0cc29a"
+            url: "https://github.com/devrev/devrev-sdk-ios/releases/download/v2.3.2/DevRevSDK.xcframework.zip",
+            checksum: "d1083ea1d23801782f440be674a6f721f4b0d844f9f91d3370761f3f2ce19140"
         )
     ]
 )
